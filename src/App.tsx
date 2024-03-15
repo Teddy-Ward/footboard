e23b537324ad8b565f4dd85f6895eaf17e0d86a3
Ted Ward <teddyward@gmail.com>
2024-03-15 15:48:02 +0000

    app creation and theme addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+import './index'; 
 
-function App() {
+const App: React.FC = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${theme === 'light' ? 'light-theme' : 'dark-theme'}`}>
+      {/* Your component content */}
+      TEST
+      <button onClick={toggleTheme}>Toggle Theme</button> 
     </div>
   );
-}
+};
 
 export default App;
